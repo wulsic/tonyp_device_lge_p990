6a28ef44b27b983690225f7cdcb70813eaa95400
Wulsic <wulsic@hotmail.com>
2014-12-19 14:12:34 +0100

    Fix the redefinition error for struct audio_config

--- include/hardware/audio.h
+++ include/hardware/audio.h
@@ -175,13 +175,6 @@ __BEGIN_DECLS
 /**************************************/
 
 /* common audio stream configuration parameters */
-struct audio_config {
-    uint32_t sample_rate;
-    audio_channel_mask_t channel_mask;
-    audio_format_t  format;
-    audio_offload_info_t offload_info;
-};
-
 typedef struct audio_config audio_config_t;
 
 #ifdef QCOM_HARDWARE
